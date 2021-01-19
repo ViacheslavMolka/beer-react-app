f6c4011434d24e7f0e8ff0381ccda35b61242163
ViacheslavMolka <molka291@rambler.ru>
2021-01-19 14:43:42 +0200

    first commit

---
+++ src/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Spinner = () => {
+    return(
+        <div class="loadingio-spinner-gear-94zfe5yslyq">
+            <div class="ldio-2p6goqfwv49">
+                <div><div></div><div></div><div></div>
+                <div></div><div></div><div></div></div>
+            </div>
+        </div>
+    )
+};
+
+export default Spinner;
